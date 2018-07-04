3910a3e5bd1015193a04fd5ee8555badd088c2de
ifa <skholifah215@gmail.com>
2018-07-05 06:15:24 +0700

    pembaruan database

--- surat_db.sql
+++ surat_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2018 at 02:51 PM
+-- Generation Time: Jul 05, 2018 at 01:08 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -25,53 +25,49 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_user`
+-- Table structure for table `organisasi`
 --
 
-CREATE TABLE `detail_user` (
-  `id_detail` int(10) NOT NULL,
-  `id_user` int(10) NOT NULL,
-  `nama_sekretaris` varchar(225) NOT NULL,
-  `nim` int(225) NOT NULL,
-  `jurusan` varchar(225) NOT NULL,
-  `prodi` varchar(225) NOT NULL,
-  `nomer_hp` varchar(225) NOT NULL,
-  `kelas` varchar(255) NOT NULL
+CREATE TABLE `organisasi` (
+  `id_organisasi` int(10) NOT NULL,
+  `nama_organisasi` varchar(225) NOT NULL,
+  `foto` varchar(225) NOT NULL,
+  `usename` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `level` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `organisasi`
+--
+
+INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `foto`, `usename`, `password`, `level`) VALUES
+(1, 'Workshop dan Riset Inormatika', 'wri.jpg', 'wri', 'wri', 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `surat_keluar`
+-- Table structure for table `surat`
 --
 
-CREATE TABLE `surat_keluar` (
+CREATE TABLE `surat` (
   `id_surat` int(10) NOT NULL,
   `id_user` int(10) NOT NULL,
-  `penerima` varchar(225) NOT NULL,
-  `nomor_surat` int(225) NOT NULL,
+  `oleh` varchar(225) NOT NULL,
+  `nomor_surat` varchar(225) NOT NULL,
   `perihal` varchar(225) NOT NULL,
   `lampiran` varchar(225) NOT NULL,
   `keterangan` varchar(225) NOT NULL,
-  `gambar` varchar(255) NOT NULL
+  `gambar` varchar(255) NOT NULL,
+  `status` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `surat_masuk`
+-- Dumping data for table `surat`
 --
 
-CREATE TABLE `surat_masuk` (
-  `id_surat` int(10) NOT NULL,
-  `id_user` int(10) NOT NULL,
-  `pengirim` varchar(225) NOT NULL,
-  `nomor_surat` int(225) NOT NULL,
-  `perihal` varchar(225) NOT NULL,
-  `lampiran` varchar(225) NOT NULL,
-  `keterangan` varchar(300) NOT NULL,
-  `gambar` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `surat` (`id_surat`, `id_user`, `oleh`, `nomor_surat`, `perihal`, `lampiran`, `keterangan`, `gambar`, `status`) VALUES
+(1, 2, 'karang taruna', '22/33/ukm/99', 'undangan rapat', '-', '3 delegasi', 'undangan rapat.jpg', 'masuk');
 
 -- --------------------------------------------------------
 
@@ -81,11 +77,13 @@ CREATE TABLE `surat_masuk` (
 
 CREATE TABLE `user` (
   `id_user` int(10) NOT NULL,
-  `nama_organisasi` varchar(225) NOT NULL,
-  `foto` varchar(225) NOT NULL,
-  `usename` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `level` varchar(225) NOT NULL
+  `id_organisasi` int(10) NOT NULL,
+  `nama_sekretaris` varchar(225) NOT NULL,
+  `nim` int(225) NOT NULL,
+  `jurusan` varchar(225) NOT NULL,
+  `prodi` varchar(225) NOT NULL,
+  `nomer_hp` varchar(225) NOT NULL,
+  `kelas` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -93,48 +91,37 @@ CREATE TABLE `user` (
 --
 
 --
--- Indexes for table `detail_user`
+-- Indexes for table `organisasi`
 --
-ALTER TABLE `detail_user`
-  ADD PRIMARY KEY (`id_detail`);
+ALTER TABLE `organisasi`
+  ADD PRIMARY KEY (`id_organisasi`) USING BTREE;
 
 --
--- Indexes for table `surat_keluar`
+-- Indexes for table `surat`
 --
-ALTER TABLE `surat_keluar`
-  ADD PRIMARY KEY (`id_surat`);
-
---
--- Indexes for table `surat_masuk`
---
-ALTER TABLE `surat_masuk`
+ALTER TABLE `surat`
   ADD PRIMARY KEY (`id_surat`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`) USING BTREE;
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `detail_user`
+-- AUTO_INCREMENT for table `organisasi`
 --
-ALTER TABLE `detail_user`
-  MODIFY `id_detail` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `organisasi`
+  MODIFY `id_organisasi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `surat_keluar`
+-- AUTO_INCREMENT for table `surat`
 --
-ALTER TABLE `surat_keluar`
-  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `surat_masuk`
---
-ALTER TABLE `surat_masuk`
-  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `surat`
+  MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `user`
 --
