5b003f0c18c93d403971f3eaa464c8909bc5564b
aisy <aisymuhammad@gmail.com>
2018-07-05 06:56:09 +0700

    add new database

--- data/surat_db (1).sql
+++ data/surat_db (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2018 at 01:08 AM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 5.6.31
+-- Waktu pembuatan: 05 Jul 2018 pada 01.51
+-- Versi server: 10.1.32-MariaDB
+-- Versi PHP: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,29 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `organisasi`
+-- Struktur dari tabel `organisasi`
 --
 
 CREATE TABLE `organisasi` (
   `id_organisasi` int(10) NOT NULL,
   `nama_organisasi` varchar(225) NOT NULL,
-  `foto` varchar(225) NOT NULL,
-  `usename` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL,
-  `level` varchar(225) NOT NULL
+  `foto` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `organisasi`
+-- Dumping data untuk tabel `organisasi`
 --
 
-INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `foto`, `usename`, `password`, `level`) VALUES
-(1, 'Workshop dan Riset Inormatika', 'wri.jpg', 'wri', 'wri', 'admin');
+INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `foto`) VALUES
+(1, 'Workshop dan Riset Inormatika', 'wri.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `surat`
+-- Struktur dari tabel `surat`
 --
 
 CREATE TABLE `surat` (
@@ -63,7 +60,7 @@ CREATE TABLE `surat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `surat`
+-- Dumping data untuk tabel `surat`
 --
 
 INSERT INTO `surat` (`id_surat`, `id_user`, `oleh`, `nomor_surat`, `perihal`, `lampiran`, `keterangan`, `gambar`, `status`) VALUES
@@ -72,14 +69,16 @@ INSERT INTO `surat` (`id_surat`, `id_user`, `oleh`, `nomor_surat`, `perihal`, `l
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int(10) NOT NULL,
   `id_organisasi` int(10) NOT NULL,
+  `level` varchar(30) NOT NULL,
   `nama_sekretaris` varchar(225) NOT NULL,
   `nim` int(225) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `jurusan` varchar(225) NOT NULL,
   `prodi` varchar(225) NOT NULL,
   `nomer_hp` varchar(225) NOT NULL,
@@ -87,46 +86,56 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `id_organisasi`, `level`, `nama_sekretaris`, `nim`, `password`, `jurusan`, `prodi`, `nomer_hp`, `kelas`) VALUES
+(1, 2, 'user', 'ifa kholif', 1241180090, '123456', 'Teknologi Informasi', 'Teknik Informatika', '000089', 'TI-2F');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `organisasi`
+-- Indeks untuk tabel `organisasi`
 --
 ALTER TABLE `organisasi`
   ADD PRIMARY KEY (`id_organisasi`) USING BTREE;
 
 --
--- Indexes for table `surat`
+-- Indeks untuk tabel `surat`
 --
 ALTER TABLE `surat`
   ADD PRIMARY KEY (`id_surat`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `organisasi`
+-- AUTO_INCREMENT untuk tabel `organisasi`
 --
 ALTER TABLE `organisasi`
   MODIFY `id_organisasi` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `surat`
+-- AUTO_INCREMENT untuk tabel `surat`
 --
 ALTER TABLE `surat`
   MODIFY `id_surat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
