34b4c2e7299a347c9c7295387a878154dd0aeaac
aisy <aisymuhammad@gmail.com>
2018-07-05 06:19:57 +0700

    fix merge

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -19,9 +19,11 @@ class Users extends CI_Controller {
 
     public function Login()
     {
-        $this->load->library('form_validation');
+		$this->load->library('form_validation');
+		
  		$this->form_validation->set_rules('username','Username','trim|required');
- 		$this->form_validation->set_rules('password','Password','trim|required|callback_cekDb');
+		$this->form_validation->set_rules('password','Password','trim|required|callback_cekDb');
+		 
  		if ($this->form_validation->run() == FALSE) {
  			$this->load->view('login/login');
  		} else {
@@ -31,24 +33,28 @@ class Users extends CI_Controller {
     }
     public function cekDb($password)
  	{
- 		$this->load->model('users_model');
- 		$username = $this->input->post('username');
- 		$result = $this->users_model->login($username,$password);
- 		if($result){
- 			$sess_array = array();
- 			foreach ($result as $row) {
- 				$sess_array = array(
- 					'id'=>$row->id,
- 					'username'=> $row->username,
- 					'level' => $row->level
- 				);
- 				$this->session->set_userdata('logged_in',$sess_array);
- 			}
- 			return true;
- 		}else{
- 			$this->form_validation->set_message('cekDb',"Login Gagal Username dan Password Tidak Valid");
- 			return false;
- 		}
+		$this->load->model('users_model');
+
+		$username = $this->input->post('username'); 
+		// $result = $this->users_model->login($username,$password);
+
+		print_r($password);
+		 
+ 		// if($result){
+ 		// 	$sess_array = array();
+ 		// 	foreach ($result as $row) {
+ 		// 		$sess_array = array(
+ 		// 			'id'=>$row->id,
+ 		// 			'username'=> $row->username,
+ 		// 			'level' => $row->level
+ 		// 		);
+ 		// 		$this->session->set_userdata('logged_in',$sess_array);
+ 		// 	}
+ 		// 	return true;
+ 		// }else{
+ 		// 	$this->form_validation->set_message('cekDb',"Login Gagal Username dan Password Tidak Valid");
+ 		// 	return false;
+ 		// }
  	}
 
 }
--- application/views/Login/Login.php
+++ application/views/Login/Login.php
@@ -12,14 +12,14 @@
                     </h1>       
                 </div>
                 <div class="col-md-6 justify-content-md-center">
-                    <form action="<?php echo base_url('index.php/Users/Login') ?>" method="post">
+                    <form action="<?php echo base_url('Users/Login') ?>" method="post">
                         <div class="form-group">
-                        <label class="white" for="username">Username</label>
-                        <input type="text" name="username" id="username" class="form-control" placeholder="username" aria-describedby="helpId">
+                            <label class="white" for="username">Username</label>
+                            <input type="text" name="username" id="username" class="form-control" placeholder="username" aria-describedby="helpId">
                         </div>
                         <div class="form-group">
-                        <label class="white" for="password">Password</label>
-                        <input type="password" name="password" id="password" class="form-control" placeholder="password" aria-describedby="helpId">
+                            <label class="white" for="password">Password</label>
+                            <input type="password" name="password" id="password" class="form-control" placeholder="password" aria-describedby="helpId">
                         </div>
                         <button type="submit" name="btn" id="" class="btn btn-success btn-lg btn-block">Login</button>
                     </form>
